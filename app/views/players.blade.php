9a31d6b8d0ba64bc0096dbafdca769a0f025cd9c
Maxime Pelletier <pelletiermaxime@gmail.com>
2013-11-03 03:21:42 -0500

    Start rewriting the search player form

--- app/views/players.blade.php
+++ app/views/players.blade.php
@@ -1,4 +1,33 @@
 @extends('layouts.base')
 @section('body')
-TEST
+{{ Form::open(array('route' => 'index', 'method' => 'GET')) }}
+<table border="0" align="center" cellspacing="3" cellpadding="3">
+	<tr>
+		<td align="right">{{ Form::label('player', 'Player name :') }}</td>
+		<td align="right" style="width:180px">
+			{{ Form::text('player', Input::get('player'), array('style'=>"width:155px")) }}
+		</td>
+	</tr>
+	<tr>
+		<td align="right">Team :</td>
+		<td align="right" style="width:180px">
+		</td>
+	</tr>
+	<tr>
+		<td align="right">Position :</td>
+		<td align="right" style="width:180px">
+		</td>
+	</tr>
+	<tr>
+		<td align="right">Number to show :</td>
+		<td align="right" style="width:180px">
+		</td>
+	</tr>
+	<tr>
+		<td colspan="2" align="center">
+		{{ Form::submit('Search') }}
+		</td>
+	</tr>
+</table>
+{{ Form::close() }}
 @stop
