fcdc523a0798ad8ae7e1865f4690333995d2f430
Maxime Pelletier <pelletiermaxime@gmail.com>
2013-11-10 01:06:32 -0500

    Start of script to get the data for the Standings page.
    Decided to use a laravel wrapper for Goutte... that seems to include goutte.
     Not sure if that's a good idea yet -_-

---
+++ app/commands/FetchStandings.php
@@ -0,0 +1,128 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class FetchStandings extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'nhl:fetch-standings';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Command description.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		$teams = $this->getTeamsArray();
+		foreach($teams as $team)
+		{
+			if (strpos($team['Team'], 'NY') !== false)
+			{
+				$teamNY = str_replace('NY ', '', $team['Team']);
+				$team_id = Team::whereName($teamNY)->pluck('id');
+			}
+			else
+			{
+				$team['Team'] = str_replace('St. Louis', 'St Louis', $team['Team']);
+				$team_id = Team::whereCity($team['Team'])->pluck('id');
+			}
+			Standings::whereTeamId($team_id)->delete();
+			Standings::create([
+				'team_id' => $team_id,
+			]);
+		}
+		// var_dump($teams);
+	}
+
+	private function getTeamsArray()
+	{
+		$client = Goutte::getNewClient();
+
+		$params = ['Team', 'GP', 'W', 'L', 'OTL', 'PTS', 'ROW', 'SOW', 'SOL', 'HOME', 'ROAD', 'GF', 'GA', 'Diff', 'L10', 'Streak'];
+		$paramCount = count($params);
+
+		$crawler = $client->request('GET', 'http://espn.go.com/nhl/standings');
+		$cells = $crawler->filter('tr.evenrow td, tr.oddrow td')->extract(array('_text'));
+
+		$noParametre = 0;
+		$noTeam = 1;
+		foreach($cells as $cell) {
+			$team[$noTeam][$params[$noParametre]] = trim($cell);
+			$noParametre++;
+			if ($noParametre >= $paramCount) //Next row of table, so increment team
+			{
+				$noParametre = 0;
+				$noTeam++;
+			}
+		}
+
+		$params = ['Team', 'GP', 'W', 'L', 'OTL', 'PTS', 'PPG', 'PPO', 'PPP', 'PPGA', 'PPOA', 'PKP'];
+		$paramCount = count($params);
+
+		$crawler = $client->request('GET', 'http://espn.go.com/nhl/standings/_/type/expanded');
+		$cells = $crawler->filter('tr.evenrow td, tr.oddrow td')->extract(array('_text'));
+
+		$noParametre = 0;
+		$noTeam = 1;
+		foreach($cells as $cell)
+		{
+			$team[$noTeam][$params[$noParametre]] = trim($cell);
+			$noParametre++;
+			if ($noParametre >= $paramCount) //Next row of table, so increment team
+			{
+				$noParametre = 0;
+				$noTeam++;
+			}
+		}
+		return $team;
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+			// array('example', InputArgument::REQUIRED, 'An example argument.'),
+		);
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			// array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+		);
+	}
+
+}
--- app/config/app.php
+++ app/config/app.php
@@ -114,6 +114,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
+		'Elvedia\Goutte\Providers\GoutteServiceProvider',
 	),
 
 	/*
@@ -178,7 +179,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-
+		'Goutte'          => 'Elvedia\Goutte\Facades\Laravel\GoutteFacade',
 	),
 
 );
