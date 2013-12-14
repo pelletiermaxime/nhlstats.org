27cbe2847023bd51c37770bdf0269dcdc87ed049
Maxime Pelletier <pelletiermaxime@gmail.com>
2013-12-14 13:34:22 -0500

    More artisan commands in the setup of all tests, not just travis

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -3,6 +3,16 @@
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 	/**
+	 * Default preparation for each test
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->prepareForTests();
+	}
+
+	/**
 	 * Creates the application.
 	 *
 	 * @return Symfony\Component\HttpKernel\HttpKernelInterface
@@ -14,6 +24,18 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 		$testEnvironment = 'testing';
 
 		return require __DIR__.'/../../bootstrap/start.php';
+	}
+
+	/**
+	 * Migrates the database and set the mailer to 'pretend'.
+	 * This will cause the tests to run quickly.
+	 *
+	 */
+	private function prepareForTests()
+	{
+		Artisan::call('migrate');
+		Artisan::call('db:seed');
+		Mail::pretend(true);
 	}
 
 }
