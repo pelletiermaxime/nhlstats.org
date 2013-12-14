46ebf6d1114998ea43990931a2f3b5bc15b5ab63
Maxime Pelletier <pelletiermaxime@gmail.com>
2013-12-13 22:58:18 -0500

    Speed up standings fetch
    * Fetch the teams with the standings to remove 1 query per standing
    * Cache for an hour the Divisions as those aren't likely to change

--- app/controllers/StandingsController.php
+++ app/controllers/StandingsController.php
@@ -4,8 +4,8 @@ class StandingsController extends BaseController
 {
 	public function index()
 	{
-		$divisions = Division::all();
-		$standings = Standings::orderBy('PTS', 'DESC')->get();
+		$divisions = Division::remember(60)->get();
+		$standings = Standings::orderBy('PTS', 'DESC')->with('team')->get();
 		$data['divisions'] = $divisions;
 		$data['standings'] = $standings;
 		return View::make('standings', $data);
