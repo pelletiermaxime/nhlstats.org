a3a37af3e2aa46546ff0a9206e0d425cf78b4e2b
Maxime Pelletier <pelletiermaxime@gmail.com>
2013-12-30 19:25:15 -0500

    Larger left cell, but with text-right

--- app/views/players.blade.php
+++ app/views/players.blade.php
@@ -14,24 +14,24 @@
 	</ul>
 </div>
 <div class="row">
-	<div class="col-xs-2 col-xs-offset-4">{{ Form::label('name', 'Player name :') }}</div>
+	<div class="col-xs-4 col-xs-offset-2 text-right">{{ Form::label('name', 'Player name :') }}</div>
 	<div class="col-xs-6">
 		{{ Form::text('name', Input::get('name'), array('style'=>"width:155px")) }}
 	</div>
 </div>
 <div class="row">
-	<div class="col-xs-2 col-xs-offset-4">Team :</div>
+	<div class="col-xs-4 col-xs-offset-2 text-right">Team :</div>
 	<div class="col-xs-6">
 		{{ Form::select('team', $all_teams, $team, array('style'=>"width:155px")) }}
 	</div>
 </div>
 <div class="row">
-	<div class="col-xs-2 col-xs-offset-4">Position :</div>
+	<div class="col-xs-4 col-xs-offset-2 text-right">Position :</div>
 	<div class="col-xs-6">
 	</div>
 </div>
 <div class="row">
-	<div class="col-xs-2 col-xs-offset-4">Number to show :</div>
+	<div class="col-xs-4 col-xs-offset-2 text-right">Number to show :</div>
 	<div class="col-xs-6">
 		{{ Form::select('count', $all_counts, $count, array('style'=>"width:155px")) }}
 	</div>
