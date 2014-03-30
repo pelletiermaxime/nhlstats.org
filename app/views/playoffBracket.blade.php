6a6130f5d008518c2e2ace509ab7c2e4b21685fe
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-03-29 22:43:45 -0400

    First version of the playoff "braket" that shows the current state of the playoffs.

--- app/views/playoffBracket.blade.php
+++ app/views/playoffBracket.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.base')
 @section('body')
 
+@foreach ($games_east as $division)
+	@foreach ($division as $game)
+		<img height="35" src="{{ asset('images/SVG') }}/{{ $game['team1']->short_name }}.svg"
+			alt="{{ $game['team1']->city }} {{ $game['team1']->name }}" />
+		vs
+		<img height="35" src="{{ asset('images/SVG') }}/{{ $game['team2']->short_name }}.svg"
+			alt="{{ $game['team2']->city }} {{ $game['team2']->name }}" />
+		<br />
+	@endforeach
+@endforeach
 
+@foreach ($games_west as $division)
+	@foreach ($division as $game)
+		<img height="35" src="{{ asset('images/SVG') }}/{{ $game['team1']->short_name }}.svg"
+			alt="{{ $game['team1']->city }} {{ $game['team1']->name }}" />
+		vs
+		<img height="35" src="{{ asset('images/SVG') }}/{{ $game['team2']->short_name }}.svg"
+			alt="{{ $game['team2']->city }} {{ $game['team2']->name }}" />
+		<br />
+	@endforeach
+@endforeach
 
 @stop
