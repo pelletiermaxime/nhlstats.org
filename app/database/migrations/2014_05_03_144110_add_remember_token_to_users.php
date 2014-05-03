528ef4bb2cedcfdb1472d2e7a5e516edf43e14d6
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-05-03 14:46:21 -0400

    Add remember_token needed by 4.1.26

---
+++ app/database/migrations/2014_05_03_144110_add_remember_token_to_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddRememberTokenToUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table) {
+			$table->string('remember_token');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table) {
+			$table->dropColumn('remember_token');
+		});
+	}
+
+}
