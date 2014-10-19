e635db1a5a033da8624272c06b562d068fc37dc9
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-10-18 23:03:31 -0400

    Convert confide stuff to tabs to follow my standards for this project

--- app/database/migrations/2014_04_04_200814_confide_setup_users_table.php
+++ app/database/migrations/2014_04_04_200814_confide_setup_users_table.php
@@ -4,45 +4,45 @@ use Illuminate\Database\Schema\Blueprint;
 
 class ConfideSetupUsersTable extends Migration {
 
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        // Creates the users table
-        Schema::create('users', function(Blueprint $table)
-        {
-            $table->engine = 'InnoDB';
-            $table->increments('id');
-            $table->string('username');
-            $table->string('email');
-            $table->string('password');
-            $table->string('confirmation_code');
-            $table->boolean('confirmed')->default(false);
-            $table->timestamps();
-        });
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Creates the users table
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->engine = 'InnoDB';
+			$table->increments('id');
+			$table->string('username');
+			$table->string('email');
+			$table->string('password');
+			$table->string('confirmation_code');
+			$table->boolean('confirmed')->default(false);
+			$table->timestamps();
+		});
 
-        // Creates password reminders table
-        Schema::create('password_reminders', function(Blueprint $t)
-        {
-            $t->engine = 'InnoDB';
-            $t->string('email');
-            $t->string('token');
-            $t->timestamp('created_at');
-        });
-    }
+		// Creates password reminders table
+		Schema::create('password_reminders', function(Blueprint $t)
+		{
+			$t->engine = 'InnoDB';
+			$t->string('email');
+			$t->string('token');
+			$t->timestamp('created_at');
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('password_reminders');
-        Schema::drop('users');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('password_reminders');
+		Schema::drop('users');
+	}
 
 }
