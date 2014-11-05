7b81aa82e26350822c802549d9e3d44b10929cef
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-11-04 23:35:34 -0500

    Add option to fetch players by time on ice to fetch players with no points. Need to refactor that stuff -_-

--- app/commands/FetchPlayers.php
+++ app/commands/FetchPlayers.php
@@ -3,6 +3,7 @@
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class FetchPlayers extends Command
 {
@@ -37,7 +38,12 @@ class FetchPlayers extends Command
 	 */
 	public function fire()
 	{
-		$players = $this->getPlayersArray();
+		$toi = $this->option('TOI');
+		if ($toi !== null) {
+			$players = $this->getPlayersOnIceArray();
+		} else {
+			$players = $this->getPlayersArray();
+		}
 		$this->savePlayers($players);
 	}
 
@@ -61,6 +67,58 @@ class FetchPlayers extends Command
 			$cells = $crawler->filter('tr.evenrow td, tr.oddrow td')->extract(array('_text'));
 
 			$noParametre = 0;
+			if (count($cells) == 0) {
+				break;
+			}
+			foreach($cells as $cell)
+			{
+				$curParam = $params[$noParametre];
+				$player[$noPlayer][$curParam] = trim($cell);
+				if ($curParam == 'Player')
+				{
+					$tabPlayerName = explode(',', $cell);
+					$player[$noPlayer]['Player'] = $tabPlayerName[0];
+					$player[$noPlayer]['Pos']    = trim($tabPlayerName[1]);
+				}
+				$noParametre++;
+				if ($noParametre >= $paramCount) //Next row of table, so increment player
+				{
+					$noParametre = 0;
+					$noPlayer++;
+				}
+			}
+			echo "Page #$currentPage fetched\n";
+			sleep(2.5);
+			$currentPage++;
+		}
+		// var_dump($player);
+		return $player;
+	}
+
+	private function getPlayersOnIceArray()
+	{
+		$client = Goutte::getNewClient();
+		$startingPage = $this->argument('startingPage');
+		$endingPage   = $this->argument('endingPage');
+		$currentPage  = $startingPage;
+
+		$params = ['Rank', 'Player', 'Team', 'GP', 'G', 'A', 'P', '+/-', 'TOI/G', 'Shifts', '1', '2'];
+		$paramCount = count($params);
+		$player = [];
+		$noPlayer = 1;
+
+		echo "Fetching Players by time on ice by pages $startingPage-$endingPage\n";
+		while ($currentPage <= $endingPage)
+		{
+			$fetchCount = ($currentPage-1) * 40 + 1;
+			$regularSeasonUrl = "http://espn.go.com/nhl/statistics/player/_/stat/timeonice/sort/avgTimeOnIce/count/";
+			$crawler = $client->request('GET', $regularSeasonUrl . $fetchCount);
+			$cells = $crawler->filter('tr.evenrow td, tr.oddrow td')->extract(array('_text'));
+
+			$noParametre = 0;
+			if (count($cells) == 0) {
+				break;
+			}
 			foreach($cells as $cell)
 			{
 				$curParam = $params[$noParametre];
@@ -157,10 +215,10 @@ class FetchPlayers extends Command
 	 */
 	protected function getArguments()
 	{
-		return array(
-			array('startingPage', InputArgument::OPTIONAL, 'Fetch only a specific page.', 1),
-			array('endingPage', InputArgument::OPTIONAL, 'Fetch only a specific page.', 20),
-		);
+		return [
+			['startingPage', InputArgument::OPTIONAL, 'Stating page number to fetch.', 1],
+			['endingPage', InputArgument::OPTIONAL, 'Ending page number to fetch.', 20],
+		];
 	}
 
 	/**
@@ -170,8 +228,8 @@ class FetchPlayers extends Command
 	 */
 	protected function getOptions()
 	{
-		return array(
-			// array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
-		);
+		return [
+			['TOI', null, InputOption::VALUE_NONE, 'Fetch by time on ice, to get all players.', null],
+		];
 	}
 }
