2d9c7adf763c1ba01a80da4ed68e1b726495994d
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-02-07 01:09:23 -0500

    First steps to migrate to L5. Not working for now because Confide doesn't work in L5 and I need to update all my views to the new blade format

---
+++ app/Http/Controllers/PlayoffBracketController.php
@@ -0,0 +1,17 @@
+<?php
+
+class PlayoffBracketController extends \Controller
+{
+	public function index()
+	{
+		$gamesEast = PlayoffTeams::byConference('EAST', $round = 1);
+		$gamesWest = PlayoffTeams::byConference('WEST', $round = 1);
+		// $scores = GameScores::with(['team1', 'team2'])->get();
+		// Debugbar::info($gamesEast);
+		// Debugbar::info($gamesWest2);
+		return View::make('playoffBracket')
+			->withGamesEast($gamesEast)
+			->withGamesWest($gamesWest)
+		;
+	}
+}
--- app/config/nhlstats.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-	'currentYear' => '1415'
-];
