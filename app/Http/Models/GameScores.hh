45554e300ef65732c929ff37b93a00341d00e800
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-04-06 15:28:25 -0400

    Fonction to get game scores of 2 teams

---
+++ app/Http/Models/GameScores.hh
@@ -0,0 +1,33 @@
+<?hh namespace App\Http\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GameScores extends Model
+{
+	protected $guarded = [];
+
+	public static $rules = [];
+
+	public function team1()
+	{
+		return $this->belongsTo('App\Http\Models\Team');
+	}
+
+	public function team2()
+	{
+		return $this->belongsTo('App\Http\Models\Team');
+	}
+
+	public function betweenTeams($team1_id, $team2_id)
+	{
+		$gamesScores = GameScores::whereRaw(
+				'((team1_id = ? AND team2_id = ?) OR (team1_id = ? AND team2_id = ?))',
+				[$team1_id, $team2_id, $team2_id, $team1_id]
+			)
+			->where('year', '=', \Config::get('nhlstats.currentYear'))
+			->with(['team1', 'team2'])
+			->get()
+		;
+		return $gamesScores;
+	}
+}
