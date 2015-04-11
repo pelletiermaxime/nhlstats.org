bf0add4e002bf16a48ab342ebac49884a137eac4
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-04-11 17:51:07 -0400

    Add caching for playoff bracket and turn to hack code

---
+++ app/Http/Controllers/PlayoffBracketController.hh
@@ -0,0 +1,42 @@
+<?hh namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Models;
+
+class PlayoffBracketController extends Controller
+{
+	public function index()
+	{
+		$round = 1;
+
+		$gamesEast = \Cache::remember("gamesEast_{$round}", 60,
+			() ==> {
+				$gamesEast = Models\PlayoffTeams::byConference('EAST', $round);
+				foreach ($gamesEast as &$game) {
+					$game['regularSeasonGames'] = Models\GameScores::betweenTeams(
+						$game['team1']['id'], $game['team2']['id']
+					);
+				}
+				return $gamesEast;
+			}
+		);
+
+		$gamesWest = \Cache::remember("gamesWest_{$round}", 60,
+			() ==> {
+				$gamesWest = Models\PlayoffTeams::byConference('WEST', $round);
+
+				foreach ($gamesWest as &$game) {
+					$game['regularSeasonGames'] = Models\GameScores::betweenTeams(
+						$game['team1']['id'], $game['team2']['id']
+					);
+				}
+				return $gamesWest;
+			}
+		);
+
+		return view('playoffBracket')
+			->withGamesEast($gamesEast)
+			->withGamesWest($gamesWest)
+		;
+	}
+}
