7c64c9a2849c469fec7d42e41392bbe8a9e8b70f
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-04-12 20:10:24 -0400

    Fix menu button on low width browsers

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -34,18 +34,18 @@ else
 
 $currentRouteName = Route::currentRouteName();
 ?>
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-	<div class="navbar-header">
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-		<a class="navbar-brand" href="{{ route('index') }}">Nhlstats</a>
-	</div>
+<nav class="navbar navbar-default navbar-fixed-top">
 	<div class="container-fluid">
-		<div class="collapse navbar-collapse">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="{{ route('index') }}">Nhlstats</a>
+		</div>
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
 			@foreach ($menu as $title => $adresse)
 				{{-- Don't show link for active pages/subpages --}}
