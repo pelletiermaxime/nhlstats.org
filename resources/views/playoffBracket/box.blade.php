70ea5c87c08905ca0854233b1530769344305eb4
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-04-30 22:47:00 -0400

    Try putting the team logos in the background image instead of to the left. My CSS fu is lacking to have proper dimensions to the divs :/

--- resources/views/playoffBracket/box.blade.php
+++ resources/views/playoffBracket/box.blade.php
@@ -1,24 +1,16 @@
 <div class="game panel">
 	<div class="game_header">
-		<div class="team_name">&nbsp;</div>
 		@for ($i = 1; $i <= count($g['regularSeasonGames']); $i++)
 			<div>{{ $i }}</div>
 		@endfor
 	</div>
-	<div class="team1">
-		<div class="team_name">
-		<img src="{{ asset('images/SVG') }}/{{ $g['team1']['short_name'] }}.svg"
-			alt="{{ $g['team1']['city'] }} {{ $g['team1']['name'] }}"
-			title="{{ $g['team1']['city'] }} {{ $g['team1']['name'] }}"
-		/>
-		<span class="team_position">({{ $g['team1_position'] }})</span>
-		</div>
+	<div class="team1" style='background-image: url("{{ asset('images/SVG') }}/{{ $g['team1']['short_name'] }}.svg")'>
 		<div class="matches">
 		<? $team1_vics = $team2_vics = 0 ?>
 		@foreach ($g['regularSeasonGames'] as $noGame => $game)
 			@if ($game['team1_id'] == $g['team1']['id'])
 				@if ($game['winner'] == 'team1')
-				<div style="font-weight:bold;font-size: 14px;">
+				<div class="winner">
 				@else
 				<div>
 				@endif
@@ -26,7 +18,7 @@
 			</div>
 			@else
 				@if ($game['winner'] == 'team2')
-				<div style="font-weight:bold">
+				<div class="winner">
 				@else
 				<div>
 				@endif
@@ -39,19 +31,12 @@
 		{{ $g['wins'][$g['team1_id']] }}
 		</div>
 	</div>
-	<div class="team2">
-		<div class="team_name">
-		<img src="{{ asset('images/SVG') }}/{{ $g['team2']['short_name'] }}.svg"
-			alt="{{ $g['team2']['city'] }} {{ $g['team2']['name'] }}"
-			title="{{ $g['team2']['city'] }} {{ $g['team2']['name'] }}"
-		/>
-		<span class="team_position">({{ $g['team2_position'] }})</span>
-		</div>
+	<div class="team2" style='background-image: url("{{ asset('images/SVG') }}/{{ $g['team2']['short_name'] }}.svg")'>
 		<div class="matches">
 		@foreach ($g['regularSeasonGames'] as $noGame => $game)
 			@if ($game['team1_id'] == $g['team2']['id'])
 			@if ($game['winner'] == 'team1')
-			<div style="font-weight:bold;font-size: 14px;">
+			<div class="winner">
 			@else
 			<div>
 			@endif
@@ -59,7 +44,7 @@
 			</div>
 			@else
 			@if ($game['winner'] == 'team2')
-			<div style="font-weight:bold;font-size: 14px;">
+			<div class="winner">
 			@else
 			<div>
 			@endif
