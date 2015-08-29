f6dd10efbe93bd194d08af2a1375ec83b4c6accd
Maxime Pelletier <pelletiermaxime@gmail.com>
2015-08-29 16:50:08 -0400

    Default mysql user to root to hopefully fix travis

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
+            'username'  => env('DB_USERNAME', 'root'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
