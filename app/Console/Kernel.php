0c97b34fc7e49e422414306c1f80a301f077eeb3
Maxime Pelletier <pelletiermaxime@gmail.com>
2016-02-06 23:41:48 -0500

    Add schedule commands instead of 12 crons on the server. Hardcoded email to send output to for now, while I see what it sends.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,44 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
+        /********* Fetch scores *********/
+        $schedule->command('nhl:fetch-scores')->cron('*/5 18-24 * * *')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+
+        // Late games in the west
+        $schedule->command('nhl:fetch-scores --yesterday')->cron('*/5 0-1 * * *')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+
+        // Weekend games
+        $schedule->command('nhl:fetch-scores')->cron('*/5 13-17 * * 6-7')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+
+        // Futur games
+        $schedule->command('nhl:fetch-scores')->dailyAt('5:00')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+
+        /********* Fetch standings *********/
+        //After weekend games
+        $schedule->command('nhl:fetch-standings')->cron('0 17 * * 6-7')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+
+        $schedule->command('nhl:fetch-standings')->dailyAt('5:30')->emailOutputTo('pelletiermaxime@gmail.com');
+        $schedule->command('nhl:fetch-standings')->dailyAt('6:30')->emailOutputTo('pelletiermaxime@gmail.com');
+        $schedule->command('nhl:fetch-standings')->dailyAt('22:00')->emailOutputTo('pelletiermaxime@gmail.com');
+        $schedule->command('nhl:fetch-standings')->dailyAt('23:00')->emailOutputTo('pelletiermaxime@gmail.com');
+
+        /********* Fetch player stats *********/
+        $schedule->command('nhl:fetch-players')->dailyAt('6:13')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+        $schedule->command('nhl:fetch-players --TOI')->dailyAt('6:23')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+
+        $schedule->command('nhl:fetch-players')->dailyAt('9:01')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+        $schedule->command('nhl:fetch-players --TOI')->dailyAt('9:12')
+                 ->emailOutputTo('pelletiermaxime@gmail.com');
+
+        /********* Fetch goaler stats *********/
+        $schedule->command('nhl:fetch-goalers')->dailyAt('6:40')->emailOutputTo('pelletiermaxime@gmail.com');
     }
 }
