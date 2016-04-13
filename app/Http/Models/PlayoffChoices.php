57a5b48cabcf9893960a5ee70dee40a7e84e801a
Maxime Pelletier <pelletiermaxime@gmail.com>
2016-04-12 22:41:03 -0400

    Fix playoff choice listing page

--- app/Http/Models/PlayoffChoices.php
+++ app/Http/Models/PlayoffChoices.php
@@ -2,6 +2,7 @@
 
 namespace Nhlstats\Http\Models;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 class PlayoffChoices extends Model
@@ -24,10 +25,10 @@ class PlayoffChoices extends Model
         return $this->belongsTo('Team');
     }
 
-    public function getChoicesByUsers()
+    public static function getChoicesByUsers() : array
     {
         $userChoices = [];
-        $query = \DB::table('playoff_choices')
+        $query = DB::table('playoff_choices')
             ->join('playoff_teams', 'playoff_teams.id', '=', 'playoff_choices.playoff_team_id')
             ->join('teams', 'teams.id', '=', 'playoff_choices.winning_team_id')
             ->join('users', 'users.id', '=', 'playoff_choices.user_id')
@@ -36,8 +37,7 @@ class PlayoffChoices extends Model
         foreach ($playoffChoices as $choice) {
             $userChoices[$choice->username][] = $choice;
         }
-        // Debugbar::log($playoffChoices);
-        // Debugbar::log($userChoices);
+
         return $userChoices;
     }
 }
