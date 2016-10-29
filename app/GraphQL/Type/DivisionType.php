a6c127e31d0894699c7afb0cc949597e117bbcc1
Maxime Pelletier <pelletiermaxime@gmail.com>
2016-10-29 18:50:02 -0400

    Define a Team query with teams and their divisions. Great stuff!

---
+++ app/GraphQL/Type/DivisionType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Nhlstats\GraphQL\Type;
+
+use GraphQL\Type\Definition\Type;
+use Folklore\GraphQL\Support\Type as GraphQLType;
+
+class DivisionType extends GraphQLType
+{
+    protected $attributes = [
+        'name' => 'Division',
+        'description' => 'Divisions'
+    ];
+
+    public function fields()
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'The id of the division'
+            ],
+            'name' => [
+                'type' => Type::string(),
+                'description' => 'The name of the division'
+            ],
+            'year' => [
+                'type' => Type::int(),
+                'description' => 'The current year of the division'
+            ],
+        ];
+    }
+
+    protected function resolveNameField($root)
+    {
+        return ucwords(strtolower($root->division));
+    }
+}
---
+++ config/graphql.php
@@ -0,0 +1,118 @@
+<?php
+
+
+return [
+
+    // The prefix for routes
+    'prefix' => 'graphql',
+
+    // The routes to make GraphQL request. Either a string that will apply
+    // to both query and mutation or an array containing the key 'query' and/or
+    // 'mutation' with the according Route
+    //
+    // Example:
+    //
+    // Same route for both query and mutation
+    //
+    // 'routes' => 'path/to/query/{graphql_schema?}',
+    //
+    // or define each routes
+    //
+    // 'routes' => [
+    //     'query' => 'query/{graphql_schema?}',
+    //     'mutation' => 'mutation/{graphql_schema?}'
+    // ]
+    //
+    // you can also disable routes by setting routes to null
+    //
+    // 'routes' => null,
+    //
+    'routes' => '{graphql_schema?}',
+
+    // The controller to use in GraphQL request. Either a string that will apply
+    // to both query and mutation or an array containing the key 'query' and/or
+    // 'mutation' with the according Controller and method
+    //
+    // Example:
+    //
+    // 'controllers' => [
+    //     'query' => '\Folklore\GraphQL\GraphQLController@query',
+    //     'mutation' => '\Folklore\GraphQL\GraphQLController@mutation'
+    // ]
+    //
+    'controllers' => '\Folklore\GraphQL\GraphQLController@query',
+
+    // Any middleware for the graphql route group
+    'middleware' => [],
+
+    // The name of the default schema used when no argument is provided
+    // to GraphQL::schema() or when the route is used without the graphql_schema
+    // parameter.
+    'schema' => 'default',
+
+    // The schemas for query and/or mutation. It expects an array to provide
+    // both the 'query' fields and the 'mutation' fields. You can also
+    // provide directly an object GraphQL\Schema
+    //
+    // Example:
+    //
+    // 'schemas' => [
+    //     'default' => new Schema($config)
+    // ]
+    //
+    // or
+    //
+    // 'schemas' => [
+    //     'default' => [
+    //         'query' => [
+    //              'users' => 'App\GraphQL\Query\UsersQuery'
+    //          ],
+    //          'mutation' => [
+    //
+    //          ]
+    //     ]
+    // ]
+    //
+    'schemas' => [
+        'default' => [
+            'query' => [
+                'team' => 'Nhlstats\GraphQL\Query\TeamQuery',
+            ],
+            'mutation' => [
+                'test' => 'Nhlstats\GraphQL\Mutation\TestMutation',
+            ],
+        ],
+    ],
+
+    // The types available in the application. You can then access it from the
+    // facade like this: GraphQL::type('user')
+    //
+    // Example:
+    //
+    // 'types' => [
+    //     'user' => 'App\GraphQL\Type\UserType'
+    // ]
+    //
+    // or whitout specifying a key (it will use the ->name property of your type)
+    //
+    // 'types' => [
+    //     'App\GraphQL\Type\UserType'
+    // ]
+    //
+    'types' => [
+        'Nhlstats\GraphQL\Type\TeamType',
+        'Nhlstats\GraphQL\Type\DivisionType',
+    ],
+
+    // This callable will received every Error objects for each errors GraphQL catch.
+    // The method should return an array representing the error.
+    //
+    // Typically:
+    // [
+    //     'message' => '',
+    //     'locations' => []
+    // ]
+    //
+    'error_formatter' => ['\Folklore\GraphQL\GraphQL', 'formatError'],
+
+];
